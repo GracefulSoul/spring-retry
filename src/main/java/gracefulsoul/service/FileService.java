333d23c9e274b627df25bf9f69846c33ced5a098
GracefulSoul <GracefulSoul@github.com>
2025-02-27 13:53:18 +0900

    Add body in write method

--- src/main/java/gracefulsoul/service/FileService.java
+++ src/main/java/gracefulsoul/service/FileService.java
@@ -8,6 +8,6 @@ import org.springframework.retry.annotation.Retryable;
 public interface FileService {
 
 	@Retryable(retryFor = IOException.class, maxAttemptsExpression = "${retry.maxAttempts}", backoff = @Backoff(delayExpression = "${retry.maxDelay}"))
-	void write(String path);
+	void write(String path, String body);
 
 }
